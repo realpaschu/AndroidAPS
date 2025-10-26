36b7b52abd3929c6acff72db279d0743489f735b
realpaschu <130929200+realpaschu@users.noreply.github.com>
2025-10-26 19:29:31 +0100

    Update Objective6.kt
    Zero

--- plugins/constraints/src/main/kotlin/app/aaps/plugins/constraints/objectives/objectives/Objective6.kt
+++ plugins/constraints/src/main/kotlin/app/aaps/plugins/constraints/objectives/objectives/Objective6.kt
@@ -14,7 +14,7 @@ class Objective6(injector: HasAndroidInjector) : Objective(injector, "maxiob", R
     @Inject lateinit var constraintChecker: ConstraintsChecker
 
     init {
-        tasks.add(MinimumDurationTask(this, T.days(1).msecs()))
+        tasks.add(MinimumDurationTask(this, T.days(0).msecs()))
         tasks.add(
             object : Task(this, R.string.closedmodeenabled) {
                 override fun isCompleted(): Boolean = ApsMode.fromString(preferences.get(StringKey.LoopApsMode)) == ApsMode.CLOSED
